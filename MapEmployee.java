c8243e0f11d5b88bd3dd093d3b9016f5b4de916f
Avi7050 <avi7050416623@gmail.com>
2023-12-28 11:24:51 +0530

    new file added

---
+++ MapEmployee.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MapEmployee {
+    public static void main(String[] args) {
+        System.out.println("Only filtering the names::");
+        List<Employee> emps = Arrays.asList(new Employee(101, "Avinash", 50000), new Employee(102, "Abhishek", 40000),
+                new Employee(103, "Akash", 30000));
+        emps.stream().map(emp -> emp.getName() + " Sinha").forEach(emp -> System.out.println(emp));
+        System.out.println("Filtering and printing all objects::");
+        emps.stream().filter(emp -> emp.getSalary() > 30000).map(emp -> {
+            emp.setSalary(emp.getSalary() + 5000);
+            return emp;
+        }).forEach(emp -> System.out.println(emp));
+    }
+}
